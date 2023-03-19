cae1b54e2d872e6ab2bc436db64c66f8dcd14f52
Arkadiy <alibullterrier@gmail.com>
2023-03-19 08:07:57 +0200

    rebuild

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,38 +1,18 @@
 import { title } from 'process'
 import React from 'react'
 
-type PropsType = {
-  title: string
-  checked: boolean
-  tasks: TaskType[]
-}
 
-type TaskType={
-  id: number 
-  title: string
-  isDone: boolean
-}
   
-const Todolist = (props: PropsType) => {
-  const title = 'New TODOLIST';
-  const MapFunc = props.tasks.map((el) => {
-      return(
-                    <li>
-                      <span>{el.title}</span>
-                      <input type="checkbox" checked={el.isDone}/>
-                    </li>
-      )
-  })
+const Todolist = () => {
+ 
     return (
     <div>
-        <h1>{props.title}</h1>
+        <h1>New todo</h1>
           <input type="text" />
            <button>+</button>
             <ul>
-              {MapFunc}
-             {/* <li><span>{props.tasks[0].title}</span><input type="checkbox" checked={props.tasks[0].isDone}/></li>
-             <li><span>{props.tasks[1].title}</span><input type="checkbox" checked={props.tasks[1].isDone}/></li>
-             <li><span>{props.tasks[2].title}</span><input type="checkbox" checked={props.tasks[2].isDone}/></li> */}
+              
+            
             </ul>
            <button>all</button>
            <button>active</button>
