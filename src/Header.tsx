2705d0a83eff1b342055b5f1a6c66d24daefb2cb
Arkadiy <alibullterrier@gmail.com>
2023-05-18 08:32:39 +0300

    new changed, add callback, add style, position header menu & form list

--- src/Header.tsx
+++ src/Header.tsx
@@ -9,9 +9,11 @@ type PropsType = {
 export const Header = (props: PropsType) => {
     return (
         <header className={s.mainHeader}>
-
             <div className={s.headerMenu}>
-                <img className={s.logo} src='https://pixlok.com/wp-content/uploads/2021/12/Technology-Icon-0oikef.png' alt='technology'></img>
+                <img className={s.logo}
+                    src='https://pixlok.com/wp-content/uploads/2021/12/Technology-Icon-0oikef.png'
+                    alt='technology'>
+                </img>
                 <span className={s.logoTitle}>smart-list.com</span>
                 <div className={s.createTodo}>
                     <AddItemForm callBack={props.callBack} />
@@ -22,7 +24,6 @@ export const Header = (props: PropsType) => {
                 <div className={s.item}><a className={s.link} href="#">market</a></div>
                 <div className={s.item}><a className={s.link} href="#">api</a></div>
             </div>
-
         </header>
     )
 }
